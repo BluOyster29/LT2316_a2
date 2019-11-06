091f00cfca589f020d6acabfa587eb79a3d19d3d
Robert <gusroberth@student.gu.se>
2019-11-06 12:20:39 +0100

    Data loader for two inputs

--- Data_set_Loader.py
+++ Data_set_Loader.py
@@ -1,13 +1,19 @@
 from torch.utils.data import Dataset
 
-
-class DebatesSet(Dataset):
-    def __init__(self,x,y):
-        self.x = x
+class DebatesSets(Dataset):
+    def __init__(self,sent1,sent2, y):
+        self.sent1 = sent1
+        self.sent2 = sent2
         self.y = y
 
     def __getitem__(self, index):
-        return (self.x[index], self.y[index])
+
+        sent1 = self.sent1[index]
+        sent2 = self.sent2[index]
+        y = self.y[index]
+
+        return (sent1,sent2), y
+
 
     def __len__(self):
-        return len(self.x)
+        return len(self.sent1)
